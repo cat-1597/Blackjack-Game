63ac578ac0d6e2dbcb591b06e6730a74afc522dc
Cat <cchiong1597@gmail.com>
2022-09-05 13:19:06 -0400

    Added new array
    Added card array

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 let firstCard = 10
-let secondCard = 4
+let secondCard = 2
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
+
+
+// Create a new function called startGame() that calls renderGame()
+
 function startGame() {
+    renderGame()
+}
+
+function renderGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack!"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+}
+
+function newCard() {
+
+    let card = 9
+
+    sum += card
+    renderGame()
 }
